55a0f2f534fdfe6633509695ff72b8beafefe9d9
khanhmeoden <khanh.th200327@sis.hust.edu.vn>
2024-05-27 22:49:06 +0700

    update

--- src/components/home/SearchResult.js
+++ src/components/home/SearchResult.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NavBar from "../NavBar";
-import Search from "../Search";
-import RecentActivity from "../RecentActivity";
+import NavBar from "./NavBar";
+import Search from "./Search";
+import RecentActivity from "./RecentActivity";
 // import './SearchResult.css';
 
 const SearchResult = ({ searchResults, keyword }) => {
